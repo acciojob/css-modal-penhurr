bc698ec88f5159f5815e181feb20075b418e7cba
Madhur Panghal <148714996+penhurr@users.noreply.github.com>
2025-12-29 21:04:25 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,39 @@
 //your JS code here. If required.
+let button = document.getElementById("openModal")
+
+button.addEventListener('click',()=>{
+	if(document.querySelector('.modal')) return
+	document.body.append(modal)
+})
+
+let modal = document.createElement('div')
+modal.className = "modal"
+
+let modalContent = document.createElement('div')
+modalContent.className = "modal-content"
+
+let modalHeader = document.createElement('div')
+modalHeader.className = "modal-header"
+
+let h2 = document.createElement('h2')
+h2.textContent = 'Modal Header'
+let close = document.createElement('button')
+close.className = "close-modal"
+close.textContent = 'x'
+close.addEventListener('click',()=>modal.remove())
+modalHeader.append(h2,close)
+
+let modalBody = document.createElement('div')
+modalBody.className = "modal-body"
+
+let p = document.createElement('p')
+p.textContent = 'Modal Body Content'
+modalBody.append(p)
+
+modalContent.append(modalHeader,modalBody)
+modal.append(modalContent)
+
+modal.addEventListener('click',()=>
+		modal.remove()
+	
+)
