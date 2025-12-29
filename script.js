c72d0029621a1cdf2d819ec1f14411465439a1fb
Madhur Panghal <148714996+penhurr@users.noreply.github.com>
2025-12-29 21:30:33 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 const modal = document.getElementById("modal");
 const openBtn = document.getElementById("openModal");
-const closeBtn = document.querySelector(".close");
+const closeBtn = document.querySelector(".close-modal");
 
 openBtn.onclick = () => modal.style.display = "block";
 closeBtn.onclick = () => modal.style.display = "none";
 
-modal.onclick = (e) => {
-  if (e.target === modal) modal.style.display = "none";
+modal.onclick = () => {
+  if(document.querySelector(".modal")) {modal.style.display = "none";}
 };
