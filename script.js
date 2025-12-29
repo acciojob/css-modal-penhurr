c6fa1986450424d244a2c68c466e6d223c76e061
Madhur Panghal <148714996+penhurr@users.noreply.github.com>
2025-12-29 21:15:53 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,39 +1,10 @@
-//your JS code here. If required.
-let button = document.getElementById("openModal")
+const modal = document.getElementById("modal");
+const openBtn = document.getElementById("openModal");
+const closeBtn = document.querySelector(".close");
 
-button.addEventListener('click',()=>{
-	if(document.querySelector('.modal')) return
-	document.body.append(modal)
-})
+openBtn.onclick = () => modal.style.display = "block";
+closeBtn.onclick = () => modal.style.display = "none";
 
-let modal = document.createElement('div')
-modal.className = "modal"
-
-let modalContent = document.createElement('div')
-modalContent.className = "modal-content"
-
-let modalHeader = document.createElement('div')
-modalHeader.className = "modal-header"
-
-let h2 = document.createElement('h2')
-h2.textContent = 'Modal Header'
-let close = document.createElement('button')
-close.className = "close-modal"
-close.textContent = 'x'
-close.addEventListener('click',()=>modal.remove())
-modalHeader.append(h2,close)
-
-let modalBody = document.createElement('div')
-modalBody.className = "modal-body"
-
-let p = document.createElement('p')
-p.textContent = 'Modal Body Content'
-modalBody.append(p)
-
-modalContent.append(modalHeader,modalBody)
-modal.append(modalContent)
-
-modal.addEventListener('click',()=>
-		modal.remove()
-	
-)
+modal.onclick = (e) => {
+  if (e.target === modal) modal.style.display = "none";
+};
